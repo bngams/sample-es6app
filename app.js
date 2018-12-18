b4671ecf7b2a816100b387ff902b1fd6486718a6
bngams <n.boris@devidia.net>
2018-12-18 15:43:06 +0100

    product card component

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { ProductForm } from "./components/product-form.js";
 import { ProductList } from "./components/product-list.js";
+import { ProductCard } from "./components/product-card.js";
 
 export class App {
     constructor() {
@@ -10,6 +11,7 @@ export class App {
         console.log('App initilization');
         customElements.define('product-form', ProductForm);
         customElements.define('product-list', ProductList);
+        customElements.define('product-card', ProductCard);
     }
 }
 
--- components/product-list.js
+++ components/product-list.js
@@ -1,10 +1,15 @@
 import { AppComponent } from "./app-component.js";
+import { ProductCard } from "./product-card.js";
 
 const TEMPLATE = `<div id="product-list"></div>`;
 
 export class ProductList extends AppComponent {
     display() {
         this.innerHTML = TEMPLATE;
+    }
+
+    bindElements() {
+        this.productContainer = this.querySelector('#product-list');
     }
 
     bindEvents() {
@@ -14,7 +19,7 @@ export class ProductList extends AppComponent {
     handleProductCreateEvent() {
         document.addEventListener('create-product', (e) => {
             console.log('product to add', e.detail);
-            this.innerHTML = (`<h1>${e.detail.name}</h1>`);
+            this.productContainer.appendChild(new ProductCard(e.detail));
         })
     }
 }
